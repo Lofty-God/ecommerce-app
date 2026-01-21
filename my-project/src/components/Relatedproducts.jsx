6a2efca5b63f580752499ccdbb7f8088fe75030f
= <=>
2026-01-21 22:32:56 +0100

    Relatedproducts.jsx updated

--- my-project/src/components/Relatedproducts.jsx
+++ my-project/src/components/Relatedproducts.jsx
@@ -19,9 +19,10 @@ const Relatedproducts = ({category, subcategory}) => {
         <div className='text-center text-3xl py-2 '>
             <Title text1={'RELATED'} text2={'PRODUCTS'}  />
         </div>
+        {/* Rendering items */}
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 sm:gap-y-6' >
-            {related.map((item, index)=>(
-                <ProductItem key={index} _id={item._id} name={item.name} image={item.image} price={item.price} />
+            {related.map((item, index )=>(
+                <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
             ))}
 
         </div>
