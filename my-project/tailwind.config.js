f72198194125cea6ab5fcc10e795f607ffc2d56d
= <=>
2026-01-24 02:09:14 +0100

    Handling negative values in tailwind.config.js file

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '[-5px]': '-5px',
+      }
+    },
   },
   plugins: [],
 }
