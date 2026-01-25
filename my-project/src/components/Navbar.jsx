579957bada21a2349cc23b747a1a847d401586cf
= <=>
2026-01-25 23:45:55 +0100

    A update on Navbar.jsx

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -161,12 +161,9 @@ import { ShopContext } from '../context/ShopContext'
 
 const Navbar = () => {
     const [visible, setVisible] = useState(false);
-    const [showProfile, setShowProfile] = useState(false);
-    const { setShowSearch, getCartCount, navigate, token, setToken, setCartItems } = useContext(ShopContext);
+    const { setShowSearch, getCartCount, navigate, setCartItems } = useContext(ShopContext);
     const logout = () => {
         navigate('/login')
-        localStorage.removeItem('token')
-        setToken('')
         setCartItems({})
 
     }
@@ -194,30 +191,18 @@ const Navbar = () => {
             </ul>
             <div className='gap-3 flex items-center cursor-pointer' >
                 <img onClick={() => setShowSearch(true)} src={assets.search_icon} alt="" className='w-10' />
-                <div className='group relative cursor-pointer' >
-                    {/* <img src={assets.profile_icon} alt="" className='w-7 ' /> */}
-                    {/* {token &&
-                        <div className='z-50 absolute right-0 w-32 rounded hidden group-hover:block dropdown-menu'>
-                            <div className='flex flex-col items-center py-3 px-2 bg-slate-100 text-gray-500 ' >
-                                <p className='py-2 text-sm font-medium hover:text-black  '>My-profile</p>
-                                <p onClick={() => navigate('/orders')} className='py-2 text-sm font-medium hover:text-black  '>Orders</p>
-                                <p onClick={logout} className='py-2 text-sm font-medium hover:text-black  '>Logout</p>
-                            </div>
-                        </div>
-                    } */}
-                    {token && (
-                        <div className='group relative'>
-                            <div onClick={() => setShowProfile(true)} onMouseEnter={() => setShowProfile(true)} onMouseLeave={() => setShowProfile(false)}>
-                                <img src={assets.profile_icon} alt="profile" className='w-8 cursor-pointer' />
-                                <div className="absolute right-0 top-full mt-0 bg-slate-100 text-gray-500 w-32 shadow-lg rounded z-50">
-                                    <p className='py-2 text-sm font-medium px-4 hover:text-black'>My-profile</p>
-                                    <p onClick={() => navigate('/orders')} className='py-2 text-sm font-medium hover:text-black px-4 cursor-pointer'>Orders</p>
-                                    <p onClick={logout} className='py-2 text-sm font-medium hover:text-black px-4 cursor-pointer'>Logout</p>
-                                </div>
-                            </div>
+                <div className='group relative' >
+                    <img src={assets.profile_icon} alt="profile" className='w-8 cursor-pointer' />
+                    <div className='group-hover:block hidden absolute right-0 dropdown-menu pt-4'>
+                        <div className="flex flex-col gap-2 w-36 py-2 px-3 bg-slate-100 text-gray-500 rounded">
+                            <p className='py-2 text-sm font-medium px-4 hover:text-black'>My-profile</p>
+                            <p onClick={() => navigate('/orders')} className='py-2 text-sm font-medium hover:text-black px-4 cursor-pointer'>Orders</p>
+                            <p onClick={logout} className='py-2 text-sm font-medium hover:text-black px-4 cursor-pointer'>Logout</p>
                         </div>
 
-                    )}
+                    </div>
+
+
 
                 </div>
                 <Link to='/cart' className='flex items-center relative '>
