e7f26f1f159619dfd423af04aac2a6758a396e7c
= <=>
2026-02-20 11:18:22 +0100

    convert dollars to naira in init paystack

--- my-project/src/pages/Placeorder.jsx
+++ my-project/src/pages/Placeorder.jsx
@@ -30,7 +30,7 @@ const Placeorder = () => {
     const handler = window.PaystackPop.setup({
       key: import.meta.env.VITE_PAYSTACK_PUBLIC_KEY,
       email: formData.email,
-      amount: orderData.amount * 100,
+      amount: (orderData.amount * 100)*1347.72,
       currency: "NGN",
       ref: "" + Math.floor(Math.random() * 1000000000 + 1),
       callback: function (response) {
